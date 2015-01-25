f9c38b04214baaae59133003df2912a06d1c5b2e
David Aragon <david@quickleft.com>
2015-01-25 11:57:39 -0800

    validations and more endpoint work
    * validate presence of first and last name
    * create endpoint
    * update specs

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -5,6 +5,12 @@ class Api::V1::ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      render json: @contact
+    else
+      render json: @contact.errors, status: 400
+    end
   end
 
   def update
@@ -12,4 +18,11 @@ class Api::V1::ContactsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :phone)
+  end
+
 end
--- spec/controllers/api/v1/contacts_controller_spec.rb
+++ spec/controllers/api/v1/contacts_controller_spec.rb
@@ -1,21 +1,37 @@
 require 'rails_helper'
 
+def build_contact
+  {first_name: "Joe", last_name: "Smith", phone: "3035555555"}
+end
+
 RSpec.describe Api::V1::ContactsController, :type => :controller do
 
-  describe "GET index" do
+  describe "index" do
     it "returns http success" do
-      contact = Contact.create
+      get :index
+      expect(response.status).to be(200)
+    end
+    it "returns all contacts" do
+      contact = Contact.create(build_contact)
       get :index
       expect(assigns(:contacts)).to eq([contact])
     end
   end
 
-  # describe "GET create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "create" do
+    it "returns http success" do
+      post :create, contact: build_contact
+      expect(response).to have_http_status(:success)
+    end
+    it "creates the contact" do
+      post :create, contact: build_contact
+      expect(Contact.first.first_name).to eq("Joe")
+    end
+    it "returns the created contact" do
+      post :create, contact: build_contact
+      expect(assigns(:contact).first_name).to eq("Joe")
+    end
+  end
 
   # describe "GET update" do
   #   it "returns http success" do
